0e8d63d149551886a82be650fbf18251b8417116
Iria Luz Suarez <irialuzs@gmail.com>
2021-03-08 19:01:36 +0000

    - Added HeroBanner to the app.js - Created HeroBanner component - Added hero banner image - Added css file for the Hero Banner styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import { React } from 'react';
 import NavbarPage from './components/navbar/navbar';
-
+import HeroBanner from './components/heroBanner/heroBanner';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
         <NavbarPage/>
+        <HeroBanner/>
+
       </header>
     </div>
   );
