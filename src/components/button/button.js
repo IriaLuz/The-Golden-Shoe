adef81e93c4857fc5efd40db40509689ec392533
Iria Luz Suarez <irialuzs@gmail.com>
2021-03-08 19:04:33 +0000

    - Created button component for the Hero Banner

---
+++ src/components/button/button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const MyButton = ({variantValue, content}) => {
+    return ( 
+       <>
+       <Button variant={variantValue}>{content}</Button>{' '}
+       </> 
+     );
+};
+ 
+export default MyButton;
