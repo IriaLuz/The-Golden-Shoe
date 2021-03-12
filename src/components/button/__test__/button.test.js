35809526a0adc803b61e6e80ca14a1b6bfc846a4
Iria Luz Suarez <irialuzs@gmail.com>
2021-03-12 17:12:51 +0000

    -Created all the test necessary for MyButton component.

--- src/components/button/__test__/button.test.js
+++ src/components/button/__test__/button.test.js
@@ -10,4 +10,31 @@ describe('MyButton render', () => {
     expect(button).toHaveAttribute('class', defaultStyle);
     expect(button).toBeInTheDocument();
   });
+
+  it('should render with content= "Buy Now"', () => {
+    const content = 'Buy Now';
+    render(<MyButton content={content} />);
+    const button = screen.getByTestId('button');
+    expect(button).toHaveTextContent(content);
+  });
+
+  it('should render with variant="secondary"', () => {
+    const variant = 'danger';
+    const expectedStyle = 'btn btn-danger';
+    render(<MyButton variantValue={variant} />);
+    const button = screen.getByTestId('button');
+    expect(button).toHaveAttribute('class', expectedStyle);
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should render with variant="success" and content="click here"', () => {
+    const content = 'click here';
+    const variant = 'success';
+    const expectedStyle = 'btn btn-success';
+    render(<MyButton variantValue={variant} content={content} />);
+    const button = screen.getByTestId('button');
+    expect(button).toHaveTextContent(content);
+    expect(button).toHaveAttribute('class', expectedStyle);
+    expect(button).toBeInTheDocument();
+  });
 });
