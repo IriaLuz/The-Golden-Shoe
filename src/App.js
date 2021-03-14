e190ee538007b7b5b83773bd33236a845f445ba8
Iria Luz Suarez <irialuzs@gmail.com>
2021-03-14 14:11:57 +0000

    Feature/tgs 22 #22-Changed the name of component MyCard to ProductCards #22-Changed the name to folder and files 'card' to 'cards' #22-Created new folder 'card' and files 'card.js' and 'card.css' #22-Created a reusable component ProductCard to be able to put the numbers of cards that the client want in ProductCards #22-Update the info in app.js adding the new component name ProductCards

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import { React } from 'react';
 import NavbarPage from './components/navbar/navbar';
 import HeroBanner from './components/heroBanner/heroBanner';
-import MyCard from './components/card/card';
+import ProductCards from './components/cards/cards';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NavbarPage/>
-        <HeroBanner/>
-        <MyCard/>
+        <NavbarPage />
+        <HeroBanner />
+        <ProductCards />
       </header>
     </div>
   );
---
+++ src/components/cards/cards.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { CardDeck } from 'react-bootstrap';
+import Image1 from './images/card-1-shoe-model.jpg';
+import Image2 from './images/card-2-shoe-model.jpg';
+import Image3 from './images/card-3-shoe-model.jpg';
+import ProductCard from '../card/card';
+import './cards.css';
+
+const ProductCards = () => {
+  return (
+    <CardDeck>
+      <ProductCard
+        image={Image1}
+        title="Pro Leather"
+        textStyling="color-text"
+        cardBody="This is a wider card with supporting text below as a natural lead-in
+      to additional content. This content is a little bit longer."
+      buttonStyling="btn-gold"
+      buttonLabel="Shop Now"
+        cardFooterText="Long-lasting cushioning"
+      />
+      <ProductCard
+        image={Image2}
+        title="Safety Boots"
+        textStyling="color-text"
+        cardBody='This card has supporting text below as a natural lead-in to
+        additional content.'
+      buttonStyling="btn-gold"
+      buttonLabel="Shop Now"
+        cardFooterText="Strong and light"
+      />
+      <ProductCard
+        image={Image3}
+        title="Tree Dasher"
+        textStyling="color-text"
+        cardBody='This is a wider card with supporting text below as a natural lead-in
+        to additional content. This card has even longer content than the
+        first to show that equal height action.'
+      buttonStyling="btn-gold"
+      buttonLabel="Shop Now"
+        cardFooterText="Breezy Sneaker"
+      />
+    </CardDeck>
+  );
+};
+
+export default ProductCards;
