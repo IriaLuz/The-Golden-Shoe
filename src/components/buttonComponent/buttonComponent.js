2f2fd592bcd75f816e0e8b19666965f1d7e65fac
Iria Luz Suarez <irialuzs@gmail.com>
2021-03-15 16:25:33 +0000

    change the name of the folder and file to have the same name of the components

---
+++ src/components/buttonComponent/buttonComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CustomButton from 'react-bootstrap/Button';
+import './buttonComponent.css';
+
+const ButtonComponent = ({
+  variantValue = 'primary',
+  content,
+  customStyle
+}) => {
+  const variant = customStyle ? '' : variantValue;
+  return <CustomButton data-testid='button' bsPrefix={customStyle} variant={variant}>{content}</CustomButton>;
+};
+
+export default ButtonComponent;
