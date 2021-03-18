b20ca2d006cf5302d4546d5c6c5448c3f5e64726
Iria Luz Suarez <irialuzs@gmail.com>
2021-03-18 18:00:23 +0000

    feature/TGS-36 #36-Added grid to move the components in productPage

--- src/pages/productPage/productPage.js
+++ src/pages/productPage/productPage.js
@@ -1,41 +1,122 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import ButtonComponent from '../../components/buttonComponent/buttonComponent';
 import CustomAccordion from '../../components/customAccordion/customAccordion';
-import CustomModal from '../customModal/customModal';
-// import CustomCarousel from '../../components/customCarousel/customCarousel';
+import CustomModal from '../../components/customModal/customModal';
 import image1 from './images/card-1-shoe-model.jpg';
+import CustomCarousel from '../../components/customCarousel/customCarousel';
 import './productPage.css';
 
 const ProductPage = () => {
   return (
     <>
-      <div>
-        <h2 className="product-title">Product Name</h2>
-        <img className="product-img" src={image1} alt="shoe1" />
-        <h3 className="title-button-product">SELECT SIZE:</h3>
-        <ButtonComponent
-          customStyle="btn-shoe-size"
-          content="uk"
-          subContent="3"
-        />
-      <CustomModal/>
-      </div>
-      <div>
-        <h3 className="title-button-product">SELECT COLOR:</h3>
-        <ButtonComponent customStyle="btn-shoe-color" />
-      </div>
-      <ButtonComponent
-        className="button-cart"
-        customStyle="btn-product"
-        content="ADD TO CART"
-      />
-      <CustomAccordion
-        className="accordion-style"
-        accordionBtnTitle="Description"
-        accordionBTnBody="Our breathable, silky-smooth sneaker made with responsibly sourced eucalyptus tree fiber treads lightly in everything you do. Made in Reading UK"
-        accordionBtnListTitle="Features"
-        accordionBtnListItems="Renewable Materials"
-      />
+      <Container>
+        <Row>
+          <Col className="mr-2">
+            <Col>
+              <img className="product-img" src={image1} alt="shoe1" />
+              {/* <CustomCarousel /> */}
+            </Col>
+            <Col>
+              <CustomCarousel />
+            </Col>
+          </Col>
+          <Col xs={6}>
+            <Row>
+              <Col className="text-start">
+                <h2>Product Name</h2>
+              </Col>
+            </Row>
+            <Row>
+              <h3 className="title-button-product">SELECT SIZE:</h3>
+            </Row>
+
+            <Row>
+              <Col>
+                <ButtonComponent
+                  customStyle="btn-shoe-size"
+                  content="uk"
+                  subContent="3"
+                />
+              </Col>
+              <Col>
+                <ButtonComponent
+                  customStyle="btn-shoe-size"
+                  content="uk"
+                  subContent="3"
+                />
+              </Col>
+              <Col>
+                <ButtonComponent
+                  customStyle="btn-shoe-size"
+                  content="uk"
+                  subContent="3"
+                />
+              </Col>
+              <Col>
+                <ButtonComponent
+                  customStyle="btn-shoe-size"
+                  content="uk"
+                  subContent="3"
+                />
+              </Col>
+              <Col>
+                <ButtonComponent
+                  customStyle="btn-shoe-size"
+                  content="uk"
+                  subContent="3"
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <CustomModal />
+              </Col>
+            </Row>
+            <Row>
+              <h3 className="title-button-product">SELECT COLOR:</h3>
+            </Row>
+            <Row>
+              <Col>
+                <ButtonComponent customStyle="btn-shoe-color" />
+              </Col>
+              <Col>
+                <ButtonComponent customStyle="btn-shoe-color" />
+              </Col>
+              <Col>
+                <ButtonComponent customStyle="btn-shoe-color" />
+              </Col>
+              <Col>
+                <ButtonComponent customStyle="btn-shoe-color" />
+              </Col>
+              <Col>
+                <ButtonComponent customStyle="btn-shoe-color" />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <ButtonComponent
+                  className="button-cart"
+                  customStyle="btn-product"
+                  content="ADD TO CART"
+                />
+              </Col>
+            </Row>
+
+            <Col>
+              <CustomAccordion
+                className="accordion-style"
+                accordionBtnTitle="Description"
+                accordionBTnBody="Our breathable, silky-smooth sneaker made with responsibly sourced eucalyptus tree fiber treads lightly in everything you do. Made in Reading UK"
+                accordionBtnListTitle="Features"
+                accordionBtnListItems="Renewable Materials"
+              />
+            </Col>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
